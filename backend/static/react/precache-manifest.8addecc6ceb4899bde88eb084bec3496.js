6a61ee06fadccb205c2e1a4be35b5df261bc0632
Vincent Paris <vincentparis@vincents-mbp.myfiosgateway.com>
2020-06-19 22:31:06 -0400

    added some flask to chan1

---
+++ backend/static/react/precache-manifest.8addecc6ceb4899bde88eb084bec3496.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e91e443413ef4eca4fcc324a0f56699d",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "3ec82168ef8693a5eb68",
+    "url": "/static/react/static/css/main.d183bbb2.chunk.css"
+  },
+  {
+    "revision": "9fd84bf897b618a176ab",
+    "url": "/static/react/static/js/2.6e29a2ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/react/static/js/2.6e29a2ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ec82168ef8693a5eb68",
+    "url": "/static/react/static/js/main.803af4c6.chunk.js"
+  },
+  {
+    "revision": "b75a5b5e40f71d99a173",
+    "url": "/static/react/static/js/runtime-main.87e018e0.js"
+  }
+]);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App" style={{color:'chartreuse'}}>
     PCA
+    <p>token = {window.token}</p>
+
     </div>
 
   );
