bb19f33c8cddd5fa7711893fc256cc794d0ae280
Vincent Paris <vincentparis@vincents-mbp.myfiosgateway.com>
2020-06-22 02:20:43 -0400

    toggle test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App" style={{color:'chartreuse'}}>
-    PCA
+    <div className="App" style={{color:'magenta'}}>
+    pca app - channel 2
     <p>from flask = {window.token}</p>
 
     </div>
