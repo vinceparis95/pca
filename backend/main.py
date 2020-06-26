66674af1c2e7b63386766c816ce5a9ebc01133f6
Vincent Paris <vincentparis@vincents-mbp.myfiosgateway.com>
2020-06-26 01:18:54 -0400

    toggletest

--- backend/main.py
+++ backend/main.py
@@ -3,12 +3,11 @@ from flask_mysqldb import MySQL
 from flask_sqlalchemy import SQLAlchemy
 import os
 
-app = Flask("__main__")
-
-mysql = SQLAlchemy(app)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://"+os.environ['Administrator'] + ":" + os.environ['Photon959']+ "@" + os.environ['pcadata.cfylo90nqrvk.us-east-2.rds.amazonaws.com'] +":3306/innodb"
-
+app = Flask(__name__)
+app.config["DEBUG"] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:Photon959@localhost/pcadata"
+app.config["SQLALCHEMY_ECHO"] = True
+db = SQLAlchemy(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -16,11 +15,13 @@ def index():
         details = request.form
         firstName = details['fname']
         lastName = details['lname']
-        cur = mysql.connection.cursor()
+        cur = db.connection.cursor()
         cur.execute("INSERT INTO MyUsers(firstName, lastName) VALUES (%s, %s)", (firstName, lastName))
         db.connection.commit()
         cur.close()
         return 'success'
     return render_template('index.html')
 
-app.run(debug=True)
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
